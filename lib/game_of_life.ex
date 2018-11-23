9496675841a12d1e6be667874b67525804be64d3
Bryan Arendt <barendt@covermymeds.com>
2018-11-23 09:03:08 -0500

    init

--- lib/game_of_life.ex
+++ lib/game_of_life.ex
@@ -1,18 +1,52 @@
 defmodule GameOfLife do
-  @moduledoc """
-  Documentation for GameOfLife.
-  """
+  defmodule Cell do
+    @spec live_neighbor_count(map(), {number(), number()}) :: number()
+    def live_neighbor_count(board, {x, y} = _position) do
+      neighbors =
+        for x_offset <- -1..1, y_offset <- -1..1 do
+          if x_offset == 0 && y_offset == 0 do
+            0
+          else
+            board[{x + x_offset, y + y_offset}]
+          end
+        end
 
-  @doc """
-  Hello world.
+      neighbors
+      |> Enum.filter(&(&1 != nil))
+      |> Enum.sum()
+    end
 
-  ## Examples
+    @spec evolve(map(), {number(), number()}) :: 0 | 1
+    def evolve(board, {x, y} = position) do
+      neighbors = live_neighbor_count(board, position)
+      current_state = board[{x, y}]
 
-      iex> GameOfLife.hello
-      :world
+      cond do
+        neighbors < 2 -> 0
+        neighbors == 2 || neighbors == 3 -> 1
+        neighbors > 3 -> 0
+        current_state == 0 && neighbors == 3 -> 1
+      end
+    end
+  end
 
-  """
-  def hello do
-    :world
+  defmodule Board do
+    @spec new(integer(), integer()) :: map()
+    def new(width, length) do
+      for x <- 0..(width - 1), y <- 0..(length - 1), into: %{}, do: {{x, y}, 0}
+    end
+
+    @spec parse(binary()) :: map()
+    def parse(string) do
+      only_string = String.replace(string, ~r/[^01]/, "")
+      height = string |> String.split("\n") |> Enum.count() |> Kernel.-(1)
+      width = string |> String.split("\n") |> Enum.at(0) |> String.length()
+
+      for y <- 0..(height - 1), x <- 0..(width - 1) do
+        target_space = String.at(only_string, y * width + x)
+        %{{x, y} => (target_space == "1" && 1) || 0}
+      end
+      |> Enum.reduce(fn x, acc -> Map.merge(x, acc) end)
+    end
   end
 end
--- test/game_of_life_test.exs
+++ test/game_of_life_test.exs
@@ -2,7 +2,98 @@ defmodule GameOfLifeTest do
   use ExUnit.Case
   doctest GameOfLife
 
-  test "greets the world" do
-    assert GameOfLife.hello() == :world
+  alias GameOfLife.Board
+  alias GameOfLife.Cell
+
+  describe "Cell" do
+    test "live_neighbor_count" do
+      board =
+        Board.parse("""
+        101
+        111
+        011
+        """)
+
+      assert Cell.live_neighbor_count(board, {0, 0}) == 2
+      assert Cell.live_neighbor_count(board, {1, 0}) == 5
+      assert Cell.live_neighbor_count(board, {2, 0}) == 2
+
+      assert Cell.live_neighbor_count(board, {0, 1}) == 3
+      assert Cell.live_neighbor_count(board, {1, 1}) == 6
+      assert Cell.live_neighbor_count(board, {2, 1}) == 4
+
+      assert Cell.live_neighbor_count(board, {0, 2}) == 3
+      assert Cell.live_neighbor_count(board, {1, 2}) == 4
+      assert Cell.live_neighbor_count(board, {2, 2}) == 3
+    end
+
+    test "evolve - live cell with fewer than two live neighbours dies" do
+      board =
+        Board.parse("""
+        000
+        011
+        000
+        """)
+
+      assert Cell.evolve(board, {1, 1}) == 0
+    end
+  end
+
+  describe "Board" do
+    test "parse 4x2" do
+      str = """
+      0000
+      0110
+      """
+
+      assert %{
+               {0, 0} => 0,
+               {1, 0} => 0,
+               {2, 0} => 0,
+               {3, 0} => 0,
+               {0, 1} => 0,
+               {1, 1} => 1,
+               {2, 1} => 1,
+               {3, 1} => 0
+             } == Board.parse(str)
+    end
+
+    test "parse" do
+      str = """
+      000
+      011
+      000
+      """
+
+      assert %{
+               {0, 0} => 0,
+               {1, 0} => 0,
+               {2, 0} => 0,
+               {0, 1} => 0,
+               {1, 1} => 1,
+               {2, 1} => 1,
+               {0, 2} => 0,
+               {1, 2} => 0,
+               {2, 2} => 0
+             } == Board.parse(str)
+    end
+
+    test "will return a board" do
+      assert Board.new(3, 3) ==
+               %{
+                 {0, 0} => 0,
+                 {1, 0} => 0,
+                 {2, 0} => 0,
+                 {0, 1} => 0,
+                 {1, 1} => 0,
+                 {2, 1} => 0,
+                 {0, 2} => 0,
+                 {1, 2} => 0,
+                 {2, 2} => 0
+               }
+    end
+  end
+
+  test "any live cell with fewer than two live nieghboers dies" do
   end
 end
